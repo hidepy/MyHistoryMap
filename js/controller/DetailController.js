2acbcb4359ab874341a46b99646ad604c05ae3e8
hideyuki <hidork0222@yahoo.co.jp>
2017-05-15 00:34:23 +0900

    2017/05/15 カルーセル＋detailとcardのcss修正

--- js/controller/DetailController.js
+++ js/controller/DetailController.js
@@ -17,17 +17,17 @@
 
             // slick(carouselのやつ)の設定
             $scope.slickConfig = {
-                enabled: true,
+                //enabled: true,
                 dots: true,
                 infinite: false,
                 speed: 300,
-                slidesToShow: 5,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 responsive: [
                     {
                       breakpoint: 1024,
                       settings: {
-                        slidesToShow: 5,
+                        slidesToShow: 3,
                         slidesToScroll: 1,
                         //infinite: true,
                         dots: true

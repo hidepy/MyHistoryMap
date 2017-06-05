0aaa262f345723a0a713e59607902f869488ac71
hidepy <hidork0222@yahoo.co.jp>
2017-06-05 18:32:04 +0900

    2017/06/05 SheetsManager.jsを真面目に修正. あと、いつまでたっても終わらないので詳細タイプについては見送りに。

--- js/SheetsManager.js
+++ js/SheetsManager.js
@@ -1,9 +1,12 @@
+// Wrapper Class for Google Sheets API 
 class SheetsManager {
 
+	// Constructor
+	// クライアントID, スプレッドシートIDを受け取りもろもろ初期化する(本クラスの仕様として、インスタンス化時に)
+	//   cliend_id   : your CLIENT ID
+	//   sheet_id    : SHEET ID (https://docs.google.com/spreadsheets/d/{HERE IS SHEET ID!!}/edit#gid=0)
+	//   auto_sign_in: インスタンス化と同時にサインインする場合はtrue[OPTIONAL]
 	constructor(client_id, sheet_id, auto_sign_in){	
-
-		console.log("in SheetsManager constructor");
-
 		this.client_id = client_id;
 		this.sheet_id = sheet_id;
 		this.googleClient = {};
@@ -11,41 +14,48 @@ class SheetsManager {
 		this.DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/sheets/v4/rest"];
 		this.SCOPE = 'https://www.googleapis.com/auth/spreadsheets';
 		
-		if(auto_sign_in){
-			this.startAuth()
-				.then(
-					()=>{
-						console.log("in then before sign in");
-						this.signIn();
-					},
-					(error)=>{
-						console.log("startAuth Failure...");
-						console.log(error);
+		// 各種ライブラリの準備
+		this.loadLibraries()
+			.then(
+				()=>{
+					// ロード成功時処理
+					console.log("[constructor] in loadLibraries's then");
+
+					// 自動サインインフラグがtrueで未サインインなら
+					if(auto_sign_in && !this.isSignedIn()){
+							this.signIn();
 					}
-				)
-		}
+				},
+				(error)=>{
+					console.log("loadLibraries Failure...");
+					console.log(error);
+				}
+			)
 	}
 
-	// G認証を行う
-	startAuth(){
-		console.log("in startAuth");
+	// ---------- ここから認証関連 ----------
+	// loadLibraries
+	// 認証ライブラリ, とSheetsAPIライブラリのロードを行う.
+	// Promiseオブジェクトを返却するんで、thenすればsheets api使用可能になった直後に処理を繋げる
+	loadLibraries(){
+		console.log("[loadLibraries] driven");
 		return new Promise((resolve, reject)=>{
 			try{
+				// クライアント認証ライブラリをロード
 				gapi.load('client:auth2',
 					()=>{
-						console.log("in startAuth-> gapi.load");
+						console.log("[loadLibraries] in gapi.load");
+						// クライアント情報のセット(初期化)
 				        gapi.client.init({
 					          discoveryDocs: this.DISCOVERY_DOCS,
 					          clientId: this.client_id,
 					          scope: this.SCOPE
 					        }).then(()=> {
-					        	console.log("in then before listen");
-
-					        	// 認証OK時に駆動する関数
+					        	// 認証OK時に駆動する関数. SheetsAPIをロードする
 					        	var loadSheetsApi = ()=> {
 							        gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4')
 							        	.then(()=> {
-							        		console.log("sheets api loaded");
+							        		console.log("[loadLibraries] sheets api loaded");
 
 							        		// 再利用するんで
 							        		this.googleClient = gapi.client;
@@ -55,15 +65,15 @@ class SheetsManager {
 					        	// 認証状態を監視させる
 					        	gapi.auth2.getAuthInstance().isSignedIn.listen(function(is_signed_in){
 					        		if(is_signed_in){
-					        			console.log("signed in!!");
+					        			console.log("[loadLibraries] signed in!!");
 					        			loadSheetsApi();
 					        		}
 					        		else{
-										console.log("signed out...");
+										console.log("[loadLibraries] signed out...");
 					        		}
 					        	});
 
-					        	// ここを通った時点でログイン済かを確認. 済なら
+					        	// ここを通った時点でログイン済かを確認. 既に認証済なら早速sheets api をロードする
 					        	if(gapi.auth2.getAuthInstance().isSignedIn.get()){
 					        		loadSheetsApi();
 					        	}
@@ -72,31 +82,45 @@ class SheetsManager {
 					        });
 					},
 					(error)=>{// gapi.load failure callback
-						console.log("gapi.load error...");
+						console.log("[loadLibraries] gapi.load error...");
 						console.log(error)
 					}
 				);
 			}
 			catch(e){
-				console.log("startAuth rejected...");
+				console.log("[loadLibraries] loadLibraries rejected...");
 				reject(e);
 			}
 		});
 	}
 
+	// signIn
+	// サインインする
 	signIn(){
 		gapi.auth2.getAuthInstance().signIn();
 	}
 
+	// signOut
+	// サインアウトする
 	signOut(){
 		gapi.auth2.getAuthInstance().signOut();
 	}
 	
+	// isSignedIn
+	//  サインイン状態を返却する
 	isSignedIn(){
 		return gapi.auth2.getAuthInstance().isSignedIn.get();
 	}
+	// ---------- ここまで認証関連 ----------
+	// 認証とspreadsheetの操作を分けろよ！とのコメントがありそうですが...ごもっともです
+	// 分けると、spreadsheetだけシンプルに使いたいんだけど、という人からすると分かりにくいかな、ということで、1クラスにしてますと言い訳しておきます。
 
-	// this method returns thenable object.
+
+
+	// getValue
+	// SpreadSheetからrange_stringで指定された範囲の値を取得する. thenableなんで、値取得後に描画処理をしたい場合は、getValue.then(function(){})してください。
+	//   range_string: シートから抽出する値の範囲. "シート1!B2:D24"のような文字列を与える
+	//   戻り値はgoogleのvalues.getの値まんま. 十分使い易かったんで
     getValue(range_string){
     	// 呼出元でthenする
     	return this.googleClient.sheets.spreadsheets.values.get({
@@ -105,14 +129,17 @@ class SheetsManager {
         });
     }
 
-    // this method returns thenable object. 
+    // findValue
+    //  SpreadSheetからrange_stringで与えられた範囲のなかにfind_valueと一致するデータが存在するかを検査する
+    //   range_string: getValueと同様. シートから抽出する値の範囲
+    //   find_value  : 検索したい値. プリミティブ型を想定(文字列や数字など)
+    //   search_all  : 検索値にヒットする全ての値を取得したい場合にtrue
+    //   戻り値は配列. 1要素ごと{row, col, data}を持つ. row, colは、検索開始位置からの差分を表す. ヒットしなかった場合は配列長0
     findValue(range_string, find_value, search_all){
     	return this.getValue(range_string)
     		.then(function(response){
-    			console.log("in findvalue getvalue callback");
+    			console.log("[findvalue] getvalue's callback");
     			return new Promise(function(resolve, reject){
-    				console.log("findvalue -> getvalue callback response=");
-    				console.log(response);
 					if(!!(response && response.result && response.result.values && (response.result.values.length > 0))){
 						var result = [];
 						for(var i = 0; i < response.result.values.length; i++){
@@ -125,11 +152,11 @@ class SheetsManager {
 								}
 							}
 						}
-						console.log("findvalue -> getvalue callback-> resolve");
+						console.log("[findvalue] getvalue callback-> resolve");
 						resolve(result);
 					}
 					else{
-						console.log("findvalue -> getvalue callback-> reject");
+						console.log("[findvalue] getvalue callback-> reject");
 
 						// nodata...
 						reject();
@@ -138,23 +165,31 @@ class SheetsManager {
     		});
     }
 
-    convAlpha2Number(s){
-    	// s is MUST uppercase
-    	return s.charCodeAt(0) - 65;
-    }
-
-    convNumber2Aplha(n){
-    	return String.fromCharCode(65 + n);
-    }
-
-    // this method returns thenable object. range_string contains sheet name
-    updateValue(range_string, update_values/* as 2dim arr */){
+    // updateValue
+    // 与えられたrange_string範囲を, update_valuesの値で更新する
+    //   range_string : getValueと同様.　更新対象範囲を指定する
+    //   update_values: 2次元配列を与える. range_stringで与えた範囲と一致するだけの行, 列を持った2次元配列をセットすること
+    updateValue(range_string, update_values){
     	return this.googleClient.sheets.spreadsheets.values.update({
           spreadsheetId: this.sheet_id,
           range: range_string,
           valueInputOption: "USER_ENTERED",
           values: update_values
     	});
+    }
+
+    // convAlpha2Number
+    // 与えられたアルファベットを数値に変換する(ex: A=0, D=3)
+    //   s: 変換対象のアルファベット. 大文字想定
+    convAlpha2Number(s){
+    	return s.charCodeAt(0) - 65;
+    }
+
+    // convNumber2Aplha
+    // 与えられた数値をアルファベットに変換する. convAlpha2Numberの反対
+    //   n: 変換対象の数値
+    convNumber2Aplha(n){
+    	return String.fromCharCode(65 + n);
     }
     
 }
